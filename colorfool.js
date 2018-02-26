7c13543b405d2f85a818ba503b4fedb1088a4bf6
Matteo Manzinello <matteo@manzinello.com>
2018-02-26 14:23:42 +0100

    Sistemate stampe varie, funzionalità base di colorfool disponibile

--- colorfool.js
+++ colorfool.js
@@ -2,6 +2,7 @@
 
 const SPACE = " ";
 const COLORFOOL = "colorfool";
+const NO_STRING = "no string specified!";
 
 var Colorfool = require('./lib/');
 
@@ -14,6 +15,9 @@ var colorfool = COLORFOOL;
 if (args.length > 0) {
     var s = args.join(SPACE);
     colorfool = Colorfool.c(s);
+    console.log("string \"" + s + "\" colorfooled!");
+} else {
+    colorfool = NO_STRING;
 }
 
 console.log(colorfool);
