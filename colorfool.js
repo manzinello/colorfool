e1f0a28fca7b02229d4172354a0463a604ac421c
Matteo Manzinello <matteo@manzinello.com>
2018-02-28 09:17:02 +0100

    Aggiunti i commenti al codice

--- colorfool.js
+++ colorfool.js
@@ -8,10 +8,12 @@ const COLORHEXA = "https://www.colorhexa.com/";
 
 var Colorfool = require('./lib/');
 
+// Prendo i parametri e shifto i valori per avere tutti i parametri eccetto i 2 iniziali
 var args = process.argv;
 args.shift();
 args.shift();
 
+// Se sono stati passati parametri chiamo la c(), altrimenti stampo un messaggio di errore
 if (args.length > 0) {
     var s = args.join(SPACE);
     var colorfool = Colorfool.c(s);
@@ -22,6 +24,7 @@ if (args.length > 0) {
     console.log(NO_STRING);
 }
 
+// Metodo per prelevare l'url di dettaglio del colore
 function colorhexa(colorfool) {
     return COLORHEXA + colorfool;
 }
