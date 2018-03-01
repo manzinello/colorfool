f12e4b14e4142842719eae060ee31e46721a760e
Matteo Manzinello <matteo@manzinello.com>
2018-03-01 21:18:10 +0100

    JS funzionante! Versione browserified caricata

--- docs/cf.js
+++ docs/cf.js
@@ -1,12 +1,20 @@
 var colorfool = require('colorfool');
 
 function changed() {
+
     setTimeout(function () {
         var t = document.getElementById('c').value;
         console.log(t);
         var color = colorfool.c(t);
         console.log(color);
+        changeColor(color);
     }, 88);
+
+}
+
+function changeColor(color) {
+    document.getElementById('color').style.backgroundColor = "#" + color;
+    document.getElementById('color-string').innerHTML = "#" + color;
 }
 
 document.getElementById('c').addEventListener('keydown', changed, false);
