54dad09ad893cda0fb64cb1c2d048a85791ceb28
Matteo Manzinello <matteo@manzinello.com>
2018-03-02 09:48:51 +0100

    Aggiustato tutto il sito con risorse rinominate (cf ora JS)

---
+++ docs/cf-nobrowserified.js
@@ -0,0 +1,26 @@
+var colorfool = require('colorfool');
+
+function changed() {
+
+    setTimeout(function () {
+
+        var t = document.getElementById('c').value;
+        console.log(t);
+        var color = colorfool.c(t);
+        console.log(color);
+        changeColor(color);
+
+    }, 88);
+
+}
+
+function changeColor(color) {
+    document.getElementById('color').style.backgroundColor = "#" + color;
+    document.getElementById('color-string').innerHTML = "#" + color;
+}
+
+document.getElementById('c').addEventListener('keydown', changed, false);
+document.getElementById('c').addEventListener('paste', changed, false);
+document.getElementById('c').addEventListener('cut', changed, false);
+
+// browserify cf-nobrowserified.js -o cf.js -d
